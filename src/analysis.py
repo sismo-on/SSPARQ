b3eccc3b03e2d3a524f5bc11fb81c48b53a5f100
gilberto leite <gilbertoneto@on.br>
2025-08-21 09:48:02 -0300

    use station code in rglob

--- src/analysis.py
+++ src/analysis.py
@@ -411,7 +411,7 @@ def calculate_metrics(input_lst):
             
             file_feather_name = output_FEATHER_FILES_METRICS+network+'.'+station+'.'+evname+'.metrics.feather'
     
-            station_pwd = list(Path(WAVE_DIR).rglob('*'+year+'.'+julian_day+'*'))
+            station_pwd = list(Path(WAVE_DIR).rglob(network+'.'+station+'*'+year+'.'+julian_day+'*'))
   
             if os.path.isfile(file_feather_name):
                 pass
